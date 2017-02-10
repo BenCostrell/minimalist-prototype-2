ddc0c89457aaf0d99e8eb49a968e6d911bc52953
Ben Costrell <bencostrell@gmail.com>
2017-02-09 19:22:57 -0500

    growth -> inching around movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	public Vector3 spawnP1;
+	public Vector3 spawnP2;
+
+	// Use this for initialization
+	void Start () {
+		InitializePlayers ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Reset")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	void InitializePlayers(){
+		GameObject player1 = Instantiate (playerPrefab, spawnP1, Quaternion.identity);
+		player1.GetComponent<PlayerController> ().playerNum = 1;
+
+		GameObject player2 = Instantiate (playerPrefab, spawnP2, Quaternion.identity);
+		player2.GetComponent<PlayerController> ().playerNum = 2;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int playerNum;
+	private float expansionTimeRemaining;
+	public float growthRate;
+	public float maxExpandTime;
+	private float defaultRadius;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		defaultRadius = GetComponent<CircleCollider2D> ().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput ();
+		if (expansionTimeRemaining > 0) {
+			Expand ();
+			expansionTimeRemaining -= Time.deltaTime;
+		} else {
+			ResetScale ();
+		}
+	}
+
+	void ProcessInput(){
+		if (Input.GetButtonDown("Shalom_P" + playerNum)){
+			if (expansionTimeRemaining > 0) {
+				expansionTimeRemaining = 0;
+				MoveToEdge ();
+				ResetScale ();
+			} else {
+				expansionTimeRemaining = maxExpandTime;
+			}
+		}
+	}
+
+	void Expand(){
+		transform.localScale += growthRate * Vector3.one;
+	}
+
+	void ResetScale(){
+		transform.localScale = Vector3.one;
+	}
+
+	void MoveToEdge(){
+		Vector2 newPosition = transform.position;
+		if (playerNum == 1) {
+			newPosition.x = GetComponent<CircleCollider2D> ().bounds.max.x - defaultRadius;
+		} else if (playerNum == 2) {
+			newPosition.x = GetComponent<CircleCollider2D> ().bounds.min.x + defaultRadius;
+		}
+		//rb.MovePosition(newPosition);
+		transform.position = newPosition;
+	}
+
+}
