4d31036be7b134930055b5d17898aa1b68335d1a
Ben Costrell <bencostrell@gmail.com>
2017-02-12 15:44:47 -0500

    slug movement, contract on re-press

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,13 @@ public class PlayerController : MonoBehaviour {
 	private float contractionTimeRemaining;
 	public float maxExpansionSize;
 	public float maxExpandTime;
-	public float maxContractTime;
+	public float contractionTimeFactor;
+	private float expansionTimeElapsed;
+	private float expandedUpTo;
 	private bool expanding;
 	private bool contracting;
 	private float defaultRadius;
+	private float targetEdge;
 	private Rigidbody2D rb;
 
 	// Use this for initialization
@@ -24,36 +27,41 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		ProcessInput ();
-		if (expansionTimeRemaining > 0) {
+		if (expanding) {
 			Expand ();
-			expansionTimeRemaining -= Time.deltaTime;
-		} else if (expanding) {
-			BeginContraction ();
-		} else if (contractionTimeRemaining > 0) {
-			Contract ();
-			contractionTimeRemaining -= Time.deltaTime;
 		} else if (contracting) {
-			ResetToNeutral ();
+			Contract ();
 		}
 	}
 
 	void ProcessInput(){
 		if (Input.GetButtonDown("Shalom_P" + playerNum)){
-			if (expanding || contracting) {
-				MoveToEdge ();
-				ResetToNeutral ();
-			} else {
+			if (expanding) {
+				BeginContraction ();
+			} else if (!contracting){
 				BeginExpansion ();	
 			}
 		}
 	}
 
 	void Expand(){
+		expansionTimeRemaining -= Time.deltaTime;
 		transform.localScale = Vector3.Lerp (Vector3.one, maxExpansionSize * Vector3.one, 1 - expansionTimeRemaining / maxExpandTime);
+		expansionTimeElapsed = Mathf.Min(maxExpandTime - expansionTimeRemaining, maxExpandTime);
+		expandedUpTo = transform.localScale.x;
+		if (expansionTimeRemaining <= 0) {
+			BeginContraction ();
+		}
 	}
 
 	void Contract(){
-		transform.localScale = Vector3.Lerp (maxExpansionSize * Vector3.one, Vector3.one, 1 - contractionTimeRemaining / maxContractTime);
+		contractionTimeRemaining -= Time.deltaTime;
+		transform.localScale = Vector3.Lerp (expandedUpTo * Vector3.one, Vector3.one, 
+			1 - contractionTimeRemaining / (expansionTimeElapsed * contractionTimeFactor));
+		MoveToEdge ();
+		if (contractionTimeRemaining <= 0) {
+			ResetToNeutral ();
+		}
 	}
 
 	void BeginExpansion(){
@@ -63,9 +71,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void BeginContraction(){
+		CircleCollider2D collider = GetComponent<CircleCollider2D> ();
+		if (playerNum == 1) {
+			targetEdge = collider.bounds.max.x;
+		} else if (playerNum == 2) {
+			targetEdge = collider.bounds.min.x;
+		}
 		expanding = false;
+		expansionTimeRemaining = 0;
 		contracting = true;
-		contractionTimeRemaining = maxContractTime;
+		contractionTimeRemaining = expansionTimeElapsed * contractionTimeFactor;
+		Debug.Log (contractionTimeRemaining);
 		GetComponent<SpriteRenderer> ().color = Color.blue;
 	}
 
@@ -80,12 +96,12 @@ public class PlayerController : MonoBehaviour {
 
 	void MoveToEdge(){
 		Vector2 newPosition = transform.position;
+		CircleCollider2D collider = GetComponent<CircleCollider2D> ();
 		if (playerNum == 1) {
-			newPosition.x = GetComponent<CircleCollider2D> ().bounds.max.x - defaultRadius;
+			newPosition.x = targetEdge - collider.bounds.extents.x;
 		} else if (playerNum == 2) {
-			newPosition.x = GetComponent<CircleCollider2D> ().bounds.min.x + defaultRadius;
+			newPosition.x = targetEdge + collider.bounds.extents.x;
 		}
-		//rb.MovePosition(newPosition);
 		transform.position = newPosition;
 	}
 
